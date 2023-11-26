60fd198294b3b2afc109162e5f6eb1745a0061ac
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2023-11-25 21:10:29 -0400

    All tests implemented and passing

--- math/math.js
+++ math/math.js
@@ -1,25 +1,25 @@
 function add(x, y) {
-
+    return x + y;
 };
 
 function subtract(x, y){
-
+    return x - y;
 };
 
 function multiply(x, y) {
-
+    return x * y;
 };
 
 function divide(x, y) {
-
+    return x / y;
 };
 
 function squareRoot(x) {
-
+    return Math.sqrt(x);
 };
 
 function max(x, y) {
-
+    return Math.max(x, y);
 };
 
 module.exports = { add, subtract, multiply, divide, squareRoot, max };
--- math/math.test.js
+++ math/math.test.js
@@ -5,8 +5,27 @@ describe("Testing basic math functions", () => {
     test("Should add two numbers", () => {
         expect(add(4, 4)).toBe(8);
     });
+
+    test("Should subtract two numbers", () => {
+        expect(subtract(8, 4)).toBe(4);
+    });
+
+    test("Should multiply two numbers", () => {
+        expect(multiply(4, 4)).toBe(16);
+    });
+
+    test("Should divide two numbers", () => {
+        expect(divide(4, 4)).toBe(1);
+    });
 });
 
 describe("Testing advanced math functions", () => {
 
+    test("Should find the square root of a number", () => {
+        expect(squareRoot(16)).toBe(4);
+    });
+
+    test("Should find the max of two numbers", () => {
+        expect(max(1047, 4549)).toBe(4549);
+    });
 });
